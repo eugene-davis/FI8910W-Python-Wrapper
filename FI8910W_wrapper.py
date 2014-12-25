db26eeccfc5947966876dfaae611ff10bce26bef
Eugene Davis <eugene@eugenemdavis.net>
2014-12-25 13:52:17 -0600

    Added left, right, and stop commands

--- FI8910W_wrapper.py
+++ FI8910W_wrapper.py
@@ -1,14 +1,19 @@
 __author__ = 'Eugene Davis'
 
+import requests
+
 class FI8910W_wrapper:
 
     # User parameter to hand off to camera
     user = "";
     # Password parameter to hand off to camera
     password = ""
+    # URL to camera
+    url = ""
 
     # Constructor - sets the username and password
-    def __init(self, user, password):
+    def __init__(self, url, user, password):
+        self.url = url
         self.user = user
         self.password = password
 
@@ -16,3 +21,21 @@ class FI8910W_wrapper:
     def __genParams(self, command):
         params = {"user":self.user, "pwd":self.password, "command":command}
         return params
+
+    # Stops camera movement
+    def stop(self):
+        # Stop command is 1
+        params = self.__genParams(1)
+        requests.get(self.url, params=params)
+
+    # Moves the camera left
+    def left(self):
+        # Left is command number 6
+        params = self.__genParams(6)
+        requests.get(self.url, params=params)
+
+    # Moves the camera right
+    def right(self):
+        # Left command is 4
+        params = self.__genParams(4)
+        requests.get(self.url, params=params)
